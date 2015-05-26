be0f5a1549ab298b030ef1d6e652554eeb3cc12d
valikos <valikos.ost@gmail.com>
2015-05-26 16:32:39 +0300

    Updates for cap 3

--- lib/capistrano/gitflow.rb
+++ lib/capistrano/gitflow.rb
@@ -1,11 +1,11 @@
-require 'capistrano'
+# require 'capistrano'
 require File.join(File.dirname(__FILE__), 'gitflow', 'natcmp')
 require 'stringex'
 
 require 'capistrano/version'
 
 if defined?(Capistrano::VERSION) && Capistrano::VERSION.to_s.split('.').first.to_i >= 3
-  require 'gitflow/recipes/capistrano3'
+  load File.expand_path("tasks/whenever.rake", __FILE__)
 else
   require 'gitflow/recipes/capistrano_legacy'
 end
